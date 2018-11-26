038d18dc73eb874506c3f4c9168d3e14b3743867
Romain GONÇALVES <romain.goncalves@gmail.com>
2018-11-26 19:27:57 +0100

    Display board

--- functions.php
+++ functions.php
@@ -19,3 +19,57 @@ function printMenu(){
     println("************ Battleship ******************");
     println("Enter your choice (1-2) :");
 }
+
+function areCoordinatesValid($coord){
+    global $config;
+    list($x, $y) = coordinatesToIndex($coord);
+    return $x >= 0 && $x < $config['horizontalSize']
+        && $y >= 0 && $y < $config['verticalSize'];
+}
+
+function indexToCoordinates($x, $y){
+    return chr(ord("A") + $x) . ($y + 1);
+}
+
+function coordinatesToIndex($coord){
+    list($x, $y) = explode('', strtoupper($coord));
+    $x = ord($x) - ord('A');
+    $y = $y - 1;
+    return [$x, $y];
+}
+
+function printBoard($extraText = []){
+    global $config, $state;
+    $rowIndexSize = strlen(strval($config['verticalSize']));
+    $board = [
+        [str_repeat(" ", $rowIndexSize), "|"],
+        [str_pad("-", $rowIndexSize, " ", STR_PAD_LEFT), "-"]
+    ];
+
+    for($x = 0; $x < $config['horizontalSize']; $x++){
+        $board[0][] = chr(ord("A") + $x);
+        $board[1][] = "-";
+    }
+    $board[0][] = "|";
+    $board[1][] = "|";
+
+    for($y = 0; $y < $config['verticalSize']; $y++){
+        $row = [str_pad($y+1, $rowIndexSize, " ", STR_PAD_LEFT), "|"];
+        for($x = 0; $x < $config['horizontalSize']; $x++){
+            $row[] = $config['cells']['empty'];
+        }
+        $row[] = "|";
+        $board[] = $row;
+    }
+    $board[] = $board[1];
+
+    foreach($board as $board_row){
+        println(implode("  ", $board_row));
+    }
+    if(!empty($extraText)){
+        println("******************");
+        foreach($extraText as $row){
+            println($row);
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,14 +1,51 @@
 <?php
 
-include_once('./functions.php');
+require_once './functions.php';
 
 
-$gameHasStarted = false;
+$config = [
+    "horizontalSize" => 10,
+    "verticalSize" => 10,
+    "cells" => [
+        "empty" => "·",
+        "ship" => "⮹",
+        "hit" => "⌧",
+        "miss" => "⊙"
+    ],
+    "ships" => [
+        "Carrier" => 5,
+        "Battleship" => 4,
+        "Cruiser" => 3,
+        "Submarine" => 3,
+        "Destroyer" => 3,
+    ]
+];
+
+// Game state object :
+//  Steps :
+//      0 - Menu
+//      1 - Game setup
+//      2 - Game
+//  Player/Computer :
+//      Ships : List of ships with their chosen coordinates
+//      Moves : History of moves and results
+$state = [
+    "step" => 0,
+    "player" => [
+        "ships" => [],
+        "moves" => []
+    ],
+    "computer" => [
+        "ships" => [],
+        "moves" => []
+    ]
+];
+
 
 // Main loop
 while(true){
     cls();
-    if(!$gameHasStarted){
+    if($state['step'] === 0){
         // Print the menu
         printMenu();
 
@@ -18,7 +55,7 @@ while(true){
         switch($choice){
             // Start a new game
             case 1 :
-                $gameHasStarted = true;
+                $state['step'] = 1;
                 break;
             // Exit game
             case 2 :
@@ -26,8 +63,14 @@ while(true){
                 break 2;
         }
     }
-    else {
-        println("New game");
+    else if($state['step'] === 1) {
+        $ships = $config['ships'];
+        $nbShips = count($config['ships']);
+        $sideText = ["You have $nbShips ships to place on a {$config['horizontalSize']}x{$config['verticalSize']} board :"];
+        foreach($config['ships'] as $ship => $size){
+            $sideText[] = "\t- {$ship} : {$size} slots";
+        }
+        printBoard($sideText);
         break;
     }
 }
